24606e34d8d7766bc2c01f48622a11a062f0a614
EC2 Default User <ec2-user@ip-172-31-30-214.eu-west-2.compute.internal>
2020-02-29 16:43:09 +0000

    Added Login

--- Website.py
+++ Website.py
@@ -5,6 +5,7 @@ from flask import Flask, g,redirect, render_template, request, session, url_for
 app = Flask(__name__)
 app.secret_key = 'X9YEm3bxpoV73jQnhxvplmMNk6rGqO4d'
 db_location = './var/database.db'
+default_Avatar="https://via.placeholder.com/128"
 
 def get_db():
     db = getattr(g, '_db', None)
@@ -28,10 +29,14 @@ def init_db():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    try:
+        userCurr = session['current_user']
+    except:
+        userCurr = ''
     if request.method == 'POST':
         search = request.form['vElemSearch']
         return search_results(search)
-    return render_template('homepage.html')
+    return render_template('homepage.html', userCurr=userCurr)
 
 @app.route('/results/<vElemSearch>', methods=['GET', 'POST'])
 def search_results(vElemSearch):
@@ -53,10 +58,18 @@ def entry():
     #dElems = data.fetchall()
     return render_template('homepage.html')#, vElems=dElems)
 
+@app.route('/admin/')
+def admin():
+    db = get_db()
+    records = [(1,"Admin", "password", "https://via.placeholder.com/128")]
+    db.cursor().executemany('insert into accounts values (?,?,?,?);',records);
+    db.commit()
+    return render_template('homepage.html')#, vElems=dElems)
+
 @app.route('/species', methods=['GET', 'POST'])
 def species():
     db = get_db()
-    data = db.cursor().execute('SELECT entry FROM animals')
+    data = db.cursor().execute('SELECT user FROM accounts')
     dElems = data.fetchall()
     if request.method == 'POST':
         search = request.form['vElemSearch']
@@ -73,13 +86,60 @@ def vEntry_page(vEntry):
         return search_results(search)
     return render_template('entrypage.html', vElems=dElems)
 
-@app.errorhandler(404, methods=['GET', 'POST'])
-def page_not_found(error):
+@app.route("/signup", methods=['GET', 'POST'])
+def register():
+   valid = ''
+   check = False
+   typeOf = 'Sign up'
+   if request.method == 'POST':
+	   db = get_db()
+	   data = db.cursor().execute('''SELECT user From accounts''')
+	   data = data.fetchall()
+	   for value in (data):
+		   if(request.form['user'] == value[0]):
+			   check = True
+			   valid = 'That name has already been used'
+	   if(check != True):
+		   user = request.form['user']
+		   pw = request.form['password']
+		   if(user is not None and pw is not None):
+			   db.cursor().execute("INSERT INTO accounts(user,password,avatar) VALUES (?,?,?)", (user,pw,default_Avatar))
+			   db.commit()
+			   return redirect(url_for('.home'))
+
+   return render_template('signuppage.html', valid=valid, typeOf=typeOf)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    valid = ''
+    check = False
+    typeOf = 'Login'
     if request.method == 'POST':
-        search = request.form['vElemSearch']
-        return search_results(search)
+        user = request.form['user']
+        pw = request.form['password']
+
+        if check_auth(user, pw):
+            session['logged_in'] = True
+            session['current_user'] = user
+            return redirect(url_for('.home'))
+        else:
+            valid = 'Wrong username or password'
+
+    return render_template('signuppage.html', valid=valid, typeOf=typeOf)
+
+@app.errorhandler(404)
+def page_not_found(error):
     return render_template('errorpage.html'), 404
 
+def check_auth(user, password):
+    db = get_db()
+    data = db.cursor().execute('''SELECT user,password From accounts''')
+    data = data.fetchall()
+    for value in (data):
+        if(user == value[0] and value[1] == password):
+            return True
+
+    return False
 
 
 if __name__ == "__main__":
--- schema.sql
+++ schema.sql
@@ -9,3 +9,9 @@ CREATE TABLE animals(
     lifeSpan text,
     typeOf text
 );
+CREATE TABLE accounts(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    user text NOT NULL,
+    password text NOT NULL,
+    avatar text
+);
